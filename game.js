38f6f91927682764a9a50662550489c7a977c9c9
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-10 05:42:31 +0000

    Fixed a Major bug which allowed users to multiple click on the first go

--- game.js
+++ game.js
@@ -8,6 +8,58 @@ var level = 0;
 
 var gameRunning = false;
 var countClick = 0;
+var countingClick = 0;
+
+
+function gameOver() {
+  var wrong = new Audio('sounds/wrong.mp3');
+  wrong.play();
+  //Game Over sound
+  $('#level-title').text("Game Over, Press Any Key to Restart"); //Game Over visual //heading
+  $('body').addClass("game-over"); //background
+  setTimeout(function() {
+    $('body').removeClass("game-over");
+  }, 100);
+
+  //Restarting the Game (reinitialising the game variables - first, level, userClickPattern)
+  first = false;
+  gameRunning = false;
+  level = 0;
+  countClick = 0;
+
+  //Emptying both patterns
+  console.log("Game Over \n");
+  console.log("User Pattern: " + userClickPattern + " \n");
+  userClickPattern.length = 0; //this empties array
+  console.log("User Pattern now: " + userClickPattern + "\n");
+
+  console.log("Game Pattern: " + gamePattern + " \n");
+  gamePattern = []; //This empties array
+  console.log("Game Pattern now: " + gamePattern + " \n");
+}
+
+function animatePress(currentColour) {
+  $('#' + currentColour).addClass('pressed'); //adds clicked effect
+  setTimeout(function() {
+    $('#' + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function implementChosenSound(randomChosenColour) {
+  var sound = new Audio('sounds/' + randomChosenColour + '.mp3');
+  sound.play();
+}
+
+//This function animates the randomChosenColour to the screen
+function implementDivColour(randomChosenColour) {
+  $('#' + randomChosenColour).fadeOut(150).fadeIn(150);
+  //Fade Out, Fade In - chained together creates a 'flash' effect.
+
+  implementChosenSound(randomChosenColour);
+}
+
+
+
 
 function comparePatterns(user, game) {
   var same = true;
@@ -35,72 +87,14 @@ function comparePatterns(user, game) {
 }
 
 
-function gameOver() {
-  //Game Over sound
-  var wrong = new Audio('sounds/wrong.mp3');
-  wrong.play();
-
-  //Game Over visual
-  //heading
-  $('#level-title').text("Game Over, Press Any Key to Restart");
-
-  //background
-  $('body').addClass("game-over");
-
-  setTimeout(function() {
-    $('body').removeClass("game-over");
-  }, 100);
-
-  //Restarting the Game (reinitialising the game variables - first, level, userClickPattern)
-  first = false;
-  gameRunning = false;
-  level = 0;
-  countClick = 0;
-
-  //Emptying both patterns
-  console.log("Game Over \n");
-  console.log("User Pattern: " + userClickPattern + " \n");
-  // for (var i = 0; i < userClickPattern.length; i++) {
-  //   userClickPattern.pop();
-  //   console.log(userClickPattern + ". i is: "+i);
-  // }
-  userClickPattern.length = 0; //this empties array
-  console.log("User Pattern now: " + userClickPattern + "\n");
 
 
-  console.log("Game Pattern: " + gamePattern + " \n");
-  // for(var i=0; i<gamePattern.length; i++) {
-  //   gamePattern.pop();
-  //   console.log(gamePattern);
-  // }
-  gamePattern = []; //This empties array
-  console.log("Game Pattern now: " + gamePattern + " \n");
-
-}
-
-function animatePress(currentColour) {
-  $('#' + currentColour).addClass('pressed'); //adds clicked effect
-  setTimeout(function() {
-    $('#' + currentColour).removeClass("pressed");
-  }, 100);
-}
-
-function implementChosenSound(randomChosenColour) {
-  var sound = new Audio('sounds/' + randomChosenColour + '.mp3');
-  sound.play();
-}
-
-
-//This function animates the randomChosenColour to the screen
-function implementDivColour(randomChosenColour) {
-  $('#' + randomChosenColour).fadeOut(150).fadeIn(150);
-  //Fade Out, Fade In - chained together creates a 'flash' effect.
-
-  implementChosenSound(randomChosenColour);
-}
 
 
 function nextSequence() {
+
+  // level++; //every new seq is new/next level
+  $('#level-title').text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4); //Generate number 0 - 3
   var randomChosenColour = buttonColours[randomNumber]; //the random number chooses color
@@ -108,33 +102,43 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
   //the chosen color is added to Game Pattern,
   //which will hold the sequence of the game
-
   console.log("No: " + randomNumber + "random color " + randomChosenColour + " game pattern " + gamePattern);
 
   implementDivColour(randomChosenColour);
-  level++; //every new seq is new/next level
-  $('#level-title').text("Level " + level);
 }
 
-function flashHeading (){
+function flashHeading() {
   $('#level-title').fadeOut(200).fadeIn(200);
 
-//Unfortunately have to access CSS Directly.
-//instead of adding a class to change style.
-//This is because the original style was added to ID.
-// and.. ID overrules all. Therefore direct access was needed.
-  $('#level-title').css("color","red");
-  setTimeout(function(){
-    $('#level-title').css("color","#FEF2BF");
-  },200);
+  //Unfortunately have to access CSS Directly.
+  //instead of adding a class to change style.
+  //This is because the original style was added to ID.
+  // and.. ID overrules all. Therefore direct access was needed.
+  $('#level-title').css("color", "red");
+  setTimeout(function() {
+    $('#level-title').css("color", "#FEF2BF");
+  }, 200);
 }
+
+function nextRound() {
+  if (gameRunning) {
+    console.log("I'm in!");
+    level++;
+    nextSequence();
+    userClickPattern = [];
+
+    countClick = 0;
+  }
+} //REDUNDANT
 
 
 // Click Event Listeners
 $('div[type="button"]').click(function() {
-  if (gameRunning) {
-    countClick++; //or ++countClick ?
 
+  if (gameRunning) {
+    console.log("listener COUNT click\n " + countClick);
+    countClick++; //or ++countClick ?
+    console.log(countClick);
     //gets user input
     var userChosenColour = this.id; //stores the id (color) of the user input
     console.log(this.id);
@@ -150,41 +154,71 @@ $('div[type="button"]').click(function() {
 
 
 
-    //check and compare user input
-    var same = comparePatterns(userClickPattern, gamePattern);
+    //---- under review
+    console.log("solving double click bug");
+    console.log(gamePattern.length);
+    console.log(countClick);
+    console.log(level);
+    if (countClick > gamePattern.length) {
+      return gameOver();
+    } else {
+      //may need mouseClick > level ? gameover()
 
+      //If the user has clicked more than needed than game over.
+      //else proceede with process
+
+      //check and compare user input
+      var same = comparePatterns(userClickPattern, gamePattern);
+    }
+    console.log("Same is NOW set to: \n" + same);
+
+    console.log("Past Compare Pattern, - into if same check");
+
+    console.log("same , click , level \n" + same + " " + countClick + " " + level);
     //if same continue (nextSequence())
-    if (same) {
+    if (same) { // maybe && countClick < level
+      console.log("countClick + level \n" + countClick + " " + level);
+
       //Do nothing untill all input taken. Validation still enforced by same variable
       if (countClick === level) {
-        setTimeout(nextSequence, 800);
+
+
+        setTimeout(nextRound, 500);
+        //this instead of next seq as it was skipping next seq but initliainsing the other 2 variable
+
+
         //if user and game pattern same then carry on to nextSequence
-        userClickPattern = [];
-        countClick = 0;
+
+        console.log("Seems fine..");
         //Clear the user Pattern so they have to do whole pattern again not 1
       }
+
     } else {
       gameOver();
       //if conflict then game over
     }
 
-
   } else {
-    //alert("start the game first by pressing down any key on your keyboard");
-    //Heading Flash here
-    // Fade or change color to create flash
     flashHeading();
   }
 })
 
+
+
+
 //Main - Starter function
 $(document).keydown(function() {
   if (!first) {
+    //Level++ can be here - possibilty - BUT
+    level++ //will also have to be added end of each round/level
     nextSequence();
     first = true;
     gameRunning = true;
-  } else {
-    console.log("stop PRESSING!");
-  }
+    //ANNSWE HERE?
 
+  } else {
+
+    console.log("stop PRESSING!");
+
+  }
 });
