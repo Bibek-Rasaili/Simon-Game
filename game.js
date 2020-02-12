820ad788d431e71dfe0405e4df335392906e843c
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-12 18:20:13 +0000

    Deploy

--- game.js
+++ game.js
@@ -12,11 +12,11 @@ var countingClick = 0;
 
 
 function gameOver() {
-  var wrong = new Audio('sounds/wrong.mp3');
+  var wrong = new Audio('sounds/wrong.mp3');   //Game Over sound
   wrong.play();
-  //Game Over sound
-  $('#level-title').text("Game Over, Press Any Key to Restart"); //Game Over visual //heading
-  $('body').addClass("game-over"); //background
+
+  $('#level-title').text("Game Over, Press Any Key to Restart"); //Game Over visual - heading
+  $('body').addClass("game-over"); //- background
   setTimeout(function() {
     $('body').removeClass("game-over");
   }, 100);
@@ -28,14 +28,8 @@ function gameOver() {
   countClick = 0;
 
   //Emptying both patterns
-  console.log("Game Over \n");
-  console.log("User Pattern: " + userClickPattern + " \n");
   userClickPattern.length = 0; //this empties array
-  console.log("User Pattern now: " + userClickPattern + "\n");
-
-  console.log("Game Pattern: " + gamePattern + " \n");
   gamePattern = []; //This empties array
-  console.log("Game Pattern now: " + gamePattern + " \n");
 }
 
 function animatePress(currentColour) {
@@ -59,166 +53,116 @@ function implementDivColour(randomChosenColour) {
 }
 
 
-
-
 function comparePatterns(user, game) {
   var same = true;
 
-  console.log("Comparing... ");
-
-  //changed this to user.length from game.length
-  //as userClickedPattern is clearn after every successive "level"
+  //user.length used instead of game.length as, userClickPattern is initialised after every successive "level"
   //therefore if we had game.length, it would be comparing against null
-  //hence returning false.
+  //potential error/unnecessarily long code run
   for (var i = 0; i < user.length; i++) {
     if (user[i] === game[i]) {
       //same
-      console.log(user[i] + " U Same G " + game[i]);
       //no need to set to true as it starts with true
-      //and will only return false if conflict.
+
     } else {
-      console.log(user[i] + " U NOTSAME G " + game[i]);
       same = false //if conflict.
     }
   }
-
   return same; //returns true is same, false is conflict
-  //return true of false
 }
 
 
-
-
-
-
+//Game starts or next round, this function gets the next sequence.
 function nextSequence() {
 
-  // level++; //every new seq is new/next level
-  $('#level-title').text("Level " + level);
+  $('#level-title').text("Level " + level);  //Changes heading after "game over" heading
 
   var randomNumber = Math.floor(Math.random() * 4); //Generate number 0 - 3
   var randomChosenColour = buttonColours[randomNumber]; //the random number chooses color
 
-  gamePattern.push(randomChosenColour);
-  //the chosen color is added to Game Pattern,
-  //which will hold the sequence of the game
-  console.log("No: " + randomNumber + "random color " + randomChosenColour + " game pattern " + gamePattern);
+  gamePattern.push(randomChosenColour); //add the new sequence to gamePattern
 
   implementDivColour(randomChosenColour);
 }
 
-function flashHeading() {
-  $('#level-title').fadeOut(200).fadeIn(200);
 
-  //Unfortunately have to access CSS Directly.
-  //instead of adding a class to change style.
-  //This is because the original style was added to ID.
-  // and.. ID overrules all. Therefore direct access was needed.
-  $('#level-title').css("color", "red");
+function flashHeading() {
+  $('#level-title').fadeOut(200).fadeIn(200); //Fades out and in - impersonates flashing
+
+  $('#level-title').css("color", "red"); //Heading color to red, timeout - imitiates flashing
   setTimeout(function() {
     $('#level-title').css("color", "#FEF2BF");
   }, 200);
 }
 
-function nextRound() {
-  if (gameRunning) {
-    console.log("I'm in!");
-    level++;
-    nextSequence();
-    userClickPattern = [];
 
+function nextRound() {
+  if (gameRunning) { //This was added to negate "double click bug",
+                    // where the next seq would be affected - see Github commits on "own-spec" branch
+
+    level++;
+
+    nextSequence();
+
+    userClickPattern = [];
     countClick = 0;
   }
-} //REDUNDANT
+}
 
 
 // Click Event Listeners
 $('div[type="button"]').click(function() {
 
   if (gameRunning) {
-    console.log("listener COUNT click\n " + countClick);
-    countClick++; //or ++countClick ?
-    console.log(countClick);
+
+    countClick++;
+
     //gets user input
-    var userChosenColour = this.id; //stores the id (color) of the user input
-    console.log(this.id);
-
+    var userChosenColour = this.id;
     userClickPattern.push(userChosenColour);
-    console.log("user pattern: " + userClickPattern);
-
 
     //Implement user input sounds and visuals
     implementChosenSound(userChosenColour);
-    //this will go above sound but stays here for now
     animatePress(userChosenColour);
 
-
-
-    //---- under review
-    console.log("solving double click bug");
-    console.log(gamePattern.length);
-    console.log(countClick);
-    console.log(level);
     if (countClick > gamePattern.length) {
+      //This was introduced to solve double click bug
+      //See github commits - on own-spec
       return gameOver();
+
     } else {
-      //may need mouseClick > level ? gameover()
-
-      //If the user has clicked more than needed than game over.
-      //else proceede with process
-
-      //check and compare user input
+      //compare input to gamePattern
       var same = comparePatterns(userClickPattern, gamePattern);
     }
-    console.log("Same is NOW set to: \n" + same);
 
-    console.log("Past Compare Pattern, - into if same check");
-
-    console.log("same , click , level \n" + same + " " + countClick + " " + level);
-    //if same continue (nextSequence())
-    if (same) { // maybe && countClick < level
-      console.log("countClick + level \n" + countClick + " " + level);
-
-      //Do nothing untill all input taken. Validation still enforced by same variable
+    if (same) {
+      //if its same, it passes.
+      //if its same and = level, go to next round
       if (countClick === level) {
-
-
         setTimeout(nextRound, 500);
-        //this instead of next seq as it was skipping next seq but initliainsing the other 2 variable
-
-
-        //if user and game pattern same then carry on to nextSequence
-
-        console.log("Seems fine..");
-        //Clear the user Pattern so they have to do whole pattern again not 1
       }
 
     } else {
       gameOver();
       //if conflict then game over
     }
-
   } else {
     flashHeading();
   }
-})
+});
 
 
-
-
-//Main - Starter function
+//Main - Game starts when keydown event occurs
 $(document).keydown(function() {
+
   if (!first) {
-    //Level++ can be here - possibilty - BUT
+
     level++ //will also have to be added end of each round/level
     nextSequence();
     first = true;
     gameRunning = true;
-    //ANNSWE HERE?
 
   } else {
-
-    console.log("stop PRESSING!");
-
+    console.log("Game is already underway.");
   }
 });
